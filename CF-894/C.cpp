f6ed38042c26027f48616072045477627a3c4bf1
Rishi Mohan Jha <53253985+Rishi-Mohan@users.noreply.github.com>
2023-08-24 23:48:07 +0530

    Create C.cpp

---
+++ CF-894/C.cpp
@@ -0,0 +1,67 @@
+/*
+    author: Rishi Mohan Jha
+    created_at: 24-08-2023  08:28 PM
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+#define ll     long long
+#define ff     first
+#define ss     second
+#define pb     push_back
+#define sz(x)  ((int)(x).size())
+#define all(a) (a).begin(),(a).end()
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);}
+
+void Solve() {
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+
+	vector<int> seq;
+
+	if (v[0] != n) {
+		cout << "NO" << endl;
+		return;
+	}
+
+	vector<int> p(n + 2, 0);
+	for (auto i : v) {
+		p[1] += 1;
+		p[i + 1] -= 1;
+	}
+
+
+	int sum = 0;
+	for (int i = 1; i <= n; i++) {
+		sum += p[i];
+		if (sum != v[i - 1]) {
+			cout << "NO" << endl;
+			return;
+		}
+	}
+	cout << "YES" << endl;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);  int T = 1;
+	cin >> T;
+	while (T--) Solve();
+
+
+#ifndef ONLINE_JUDGE
+	// cout << "\nTime Elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " sec\n";
+#endif
+}
